5e188ccf2390313fb9137c4f1cabdcff0d655d6b
Edmond Wang <644568491@qq.com>
2019-03-13 17:49:05 +0800

    support image and css

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,2 +1,19 @@
 'use strict';
 
+import '../stylesheets/style.css';
+import MyLogo from '../images/logo-edmondwang.svg';
+
+console.log('Hello webpack');
+
+function component() {
+    var element = document.createElement('div');
+    element.classList.add('my');
+
+    var myLogoImg = new Image();
+    myLogoImg.src = MyLogo;
+    element.appendChild(myLogoImg);
+
+    return element;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    entry: "./public/javascripts/index.js",
+    entry: './public/javascripts/index.js',
     output: {
-        path: path.resolve(__dirname, "public-dist"),
-        filename: "my-bundle.js"
+        path: path.resolve(__dirname, 'public-dist'),
+        filename: 'my-bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader', // creates style nodes from JS strings
+                'css-loader' // translate CSS into Common JS
+            ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader'
+            ]
+        }]
     }
 };
